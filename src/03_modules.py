34ecfa3d2c7297c840a2e55515f2ae0acda969ab
unknown <dj.zaragoza@icloud.com>
2020-05-12 18:07:33 -0700

    testing

--- src/03_modules.py
+++ src/03_modules.py
@@ -13,15 +13,15 @@ import sys
 for arg in sys.argv: 
    print(arg)
 
-# print(*sys.argv, sep="\n")
+print(*sys.argv, sep="\n")
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-# print(sys.platform)
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-# print(sys.version)
+print(sys.version)
 
 
 import os
@@ -29,12 +29,12 @@ import os
 
 # Print the current process ID
 # YOUR CODE HERE
-# print(os.getpid())
+print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-# print(os.getcwd())
+print(os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
-# print(os.getlogin())
+print(os.getlogin())
